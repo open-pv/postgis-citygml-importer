302984fa78b849db0b9e6125558ef559e61a1720
Konrad Heidler <k.heidler@tum.de>
2024-05-03 09:35:08 +0200

    Adapt script for German CityGMLs

--- citygml2pgsql.py
+++ citygml2pgsql.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python
 # coding: utf-8
-"""Convert CityGML buildings to PostreSQL statements for insersion in table
 
-USAGE
-    citygml2pgsql -l file1.xml [file2.xml ...] 
-    citygml2pgsql file1.xml [file2.xml ...] <lod> srid geometry_column building_id_column table_name
-
-    <lod> can be 0, 1, 2, 3
-    srid
-
-OPTIONS
-    -h, --help display this message
-
-    -l, --list list geometry type en srs found in file
-
-"""
-
-import os
-import sys
 import re
 from lxml import etree
+import yaml
+import psycopg2
+from psycopg2.extras import execute_batch
+import argparse
+from pathlib import Path
+from tqdm import tqdm
+import pypeln.process as pl
+from munch import munchify
 
 
 def gmlLinearRing2wkt(ring, dim):
@@ -31,21 +22,21 @@ def gmlLinearRing2wkt(ring, dim):
     if coord[0] != coord[-1]:
         coord.append(coord[0]) # close ring if not closed
     if len(coord) < 4:
-        sys.stderr.write( 'degenerated LinearRing gml:id="'+\
+        print( 'degenerated LinearRing gml:id="'+\
                 ring.get("{http://www.opengis.net/gml}id")+'"\n')
         return None
     assert len(coord) >= 4
-    return "("+",".join([" ".join(c) for c in coord])+")"
+    return f"("+",".join([" ".join(c) for c in coord])+")"
 
 def gmlPolygon2wkt(poly, dim):
     dim = int(poly.get("srsDimension")) if poly.get("srsDimension") else dim
-    rings = filter(None, [gmlLinearRing2wkt(ring, dim) \
-            for ring in poly.iter("{http://www.opengis.net/gml}LinearRing") ])
+    rings = [_f for _f in [gmlLinearRing2wkt(ring, dim) \
+            for ring in poly.iter("{http://www.opengis.net/gml}LinearRing") ] if _f]
     if not rings:
-        sys.stderr.write( 'degenerated Polygon gml:id="'+\
+        print( 'degenerated Polygon gml:id="'+\
                 poly.get("{http://www.opengis.net/gml}id")+'"\n')
         return None
-    return "("+",".join(rings)+")"
+    return f"({','.join(rings)})"
 
 def findNamespaceFor(elmentName, root):
     for e in root.iter():
@@ -60,7 +51,7 @@ def fullName(elmentName, root):
     namespace = findNamespaceFor(elmentName, root)
     return namespace+elmentName if namespace else None
 
-def buildingGeomTypes(root, lods=range(3)):
+def buildingGeomTypes(root, lods=[0, 1, 2, 3]):
     types = set()
     name = fullName('Building', root)
     if not name:
@@ -74,66 +65,95 @@ def buildingGeomTypes(root, lods=range(3)):
     return types
 
 
-def citygml2pgsql(filename, table_name, srid, lod, geometry_column="geom", building_id_column="building_id"):
-    if not os.path.exists(filename):
-        raise RuntimeError("error: cannot find "+filename)
+def citygml2pgsql(filename, conf, args):
+    if not filename.exists():
+      raise RuntimeError("error: cannot find "+filename)
 
     root = etree.parse(filename)
-
     #generate a multipolygon surface per building
+    geom_types = buildingGeomTypes(root, [int(args.lod[-1])])
+    num_inserts = 0
 
-    geom_types = buildingGeomTypes(root, [lod])
-
+    tuples = []
     for building in root.iter(fullName("Building", root)):
-    	building_id = building.attrib["{http://www.opengis.net/gml}id"]
-    	building_polys = []
-        for geom_type in geom_types:
-            for geom in building.iter(geom_type):
-                dim = int(geom.get("srsDimension")) if geom.get("srsDimension") else None
-                polys = filter(None, [gmlPolygon2wkt(poly, dim) \
-                            for poly in geom.iter(fullName("Polygon", building))])
-                building_polys = building_polys + polys
-        if len(building_polys) != 0:
-            print "INSERT INTO "+table_name+"("+geometry_column + "," + building_id_column + ") VALUES ("\
-                  "'SRID="+str(srid)+\
-                    "; MULTIPOLYGON("+",".join(building_polys)+\
-                    ")'::geometry, '" + str(building_id) + "' );"
-        else:
-            sys.stderr.write( 'degenerated '+geom+' gml:id="'+\
-                    geom.get("{http://www.opengis.net/gml}id")+'"\n')
+      try:
+        building_id = building.attrib["{http://www.opengis.net/gml}id"]
+      except KeyError:
+        pass
+      building_polys = []
+      for geom_type in geom_types:
+          for geom in building.iter(geom_type):
+              dim = int(geom.get("srsDimension")) if geom.get("srsDimension") else None
+              polys = [_f for _f in [gmlPolygon2wkt(poly, dim) \
+                            for poly in geom.iter(fullName("Polygon", building))] if _f]
+              building_polys = building_polys + polys
+      if len(building_polys) == 0:
+        # Panic/Saxony mode, collect all <bldg:WallSurface> and <bldg:RoofSurface> nodes as a last resort...
+        for geom_type in ['bldg:WallSurface', 'bldg:RoofSurface']:
+          for geom in building.iter(geom_type):
+              dim = int(geom.get("srsDimension")) if geom.get("srsDimension") else None
+              polys = [_f for _f in [gmlPolygon2wkt(poly, dim) \
+                            for poly in geom.iter(fullName("Polygon", building))] if _f]
+              building_polys = building_polys + polys
+
+      if len(building_polys) != 0:
+        geom_str = f"SRID={args.srid}; MULTIPOLYGON({','.join(building_polys)})"
+        tuples.append({
+          'id': building_id,
+          'filename': filename.name,
+          'geom': geom_str
+        })
+    # conn = psycopg2.connect(database=conf.db.database, host=conf.db.host, port=conf.db.port)
+    # cur = conn.cursor()
+
+    # execute_batch(cur,
+    #   f'INSERT INTO {config.db.table} ({config.columns.id}, {config.columns.filename}, {config.columns.geometry}) '
+    #   f'VALUES (%(id)s, %(filename)s, ST_Transform(%(geom)s, {config.target_srs})) '
+    #   f'ON CONFLICT ({config.columns.id}) DO UPDATE SET '
+    #   f'{config.columns.geometry}=EXCLUDED.{config.columns.geometry}, {config.columns.filename}=EXCLUDED.{config.columns.filename}',
+    #   tuples
+    # )
+
+    # conn.commit()
+    return len(tuples)
+
 
 if __name__ == '__main__':
-    
-    if len(sys.argv) >= 2 and sys.argv[1] in ["-h","--help"]:
-        help(sys.modules[__name__])
-        exit(0)
+  with open('config.yaml', 'r') as f:
+    config = munchify(yaml.safe_load(f))
 
-    if len(sys.argv) >= 3 and sys.argv[1] in ["-l", "--list"]:
-        for filename in sys.argv[2:]:
-            srs = set()
-            print "In", filename
-            with open(filename) as xml:
-                for line in xml:
-                    m = re.match(r'.*srsName="([^"]+)"', line)
-                    if m:
-                        srs.add(m.groups()[0])
-            for srid in srs:
-                print "    found reference to", srid
-            root = etree.parse(filename)
-            for geom in buildingGeomTypes(root):
-                print "    found geometries", re.sub('.*}', '', geom)
-        exit(0)
+  parser = argparse.ArgumentParser('citygml2pgsql')
+  parser.add_argument('base_path', type=Path)
+  parser.add_argument('srid', type=int)
+  parser.add_argument('lod', type=str, choices=['lod1', 'lod2', 'lod3', 'lod4'], default='lod2')
+  args = parser.parse_args()
 
-    if len(sys.argv) < 5:
-        print "error: wrong number of arguments (try "+sys.argv[0]+" --help)"
-        exit(1)
+  print(f'Starting import for {args.base_path}')
 
-    table_name = sys.argv[-1]
-    building_id_column = sys.argv[-2]
-    geometry_column = sys.argv[-3]
-    srid = int(sys.argv[-4])
-    lod = int(sys.argv[-5])
-    for filename in sys.argv[1:-5]:
-        sys.stderr.write("converting "+filename+"\n")
-        citygml2pgsql(filename, table_name, srid, lod, geometry_column, building_id_column)
+  all_files = list(args.base_path.glob('**/*.gml'))
+  if not all_files:
+    all_files = list(args.base_path.glob('**/*.xml'))
+  print(f'Found {len(all_files)} files')
+
+  # conn = psycopg2.connect(database=config.db.database, host=config.db.host, port=config.db.port)
+  # cur = conn.cursor()
+  # cur.execute('select distinct filename from buildings;')
+  # already_read = set(i for i, in cur)
+  # conn.close()
+
+  already_read = set()
+  files = [f for f in all_files if f.name not in already_read]
+  print(f'Skipping {len(all_files) - len(files)} files that were already processed')
+
+  def process_file(filename):
+    count = citygml2pgsql(filename, config, args)
+    return filename, count
+
+  total = 0
+  # progress = tqdm(pl.map(process_file, files, workers=8), total=len(files))
+  files = files[:10]
+  progress = tqdm(map(process_file, files), total=len(files))
+  for (filename, count) in progress:
+    total += count
+    progress.set_description(f'Processing {filename.name} Total bldgs: {total}')
 
